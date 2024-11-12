1e07a454c89c93e327b248ab9bce35c9d0325238
Dead245 <116427900+Dead245@users.noreply.github.com>
2024-11-11 22:34:23 -0600

    updated player movement/look, again
    Also set Rigidbody and Character as required components for it using the [RequiredComponent()] Attribute.

--- Assets/Player Movement.cs
+++ Assets/Player Movement.cs
@@ -1,40 +1,43 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody)),RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Camera cam;
     [SerializeField] float cameraSensitivity;
     [SerializeField] float playerSpeed;
+    [SerializeField] float verticalCamClamp;
 
     CharacterController charController;
 
     private Rigidbody rb;
     private Vector2 movementInput;
     private Vector2 inputRot;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        charController = GetComponent<CharacterController>();
-        if (charController == null) { 
+        if (!TryGetComponent<CharacterController>(out charController)) { 
             Debug.LogError($"{name}'s Player Movement Script doesn't have a \"Character Controller\" component!"); 
         }
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    private void LateUpdate()
+    private void Update()
     {
+        float delta = Time.deltaTime;
         //Move Character
-        var movement = movementInput.x * transform.right + movementInput.y * transform.forward;
-        charController.Move(movement * playerSpeed * Time.deltaTime);
+        Vector3 moveDir = new Vector3(movementInput.x, 0f, movementInput.y);
+        moveDir = transform.TransformDirection(moveDir);
+        charController.Move(moveDir * playerSpeed * delta);
 
         //Rotate Camera Up/Down
-        float camRotX = cam.transform.rotation.eulerAngles.x;
-        camRotX -= inputRot.y * cameraSensitivity * Time.deltaTime;
+        float camRotX = cam.transform.rotation.eulerAngles.x - inputRot.y * cameraSensitivity * delta;
         cam.transform.localRotation = Quaternion.Euler(camRotX, 0f, 0f);
         //Rotate Player Left/Right
-        transform.Rotate(Vector3.up * inputRot.x * cameraSensitivity * Time.deltaTime);
+        transform.Rotate(Vector3.up * inputRot.x * cameraSensitivity * delta);
     }
 
     void OnLook(InputValue rotation) {
