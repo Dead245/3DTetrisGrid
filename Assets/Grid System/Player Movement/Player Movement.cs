df12eedabe3b9e3413b2b92a0e73ff1bd29921df
Dead245 <116427900+Dead245@users.noreply.github.com>
2024-11-23 15:27:14 -0600

    player now has gravity

--- Assets/Grid System/Player Movement/Player Movement.cs
+++ Assets/Grid System/Player Movement/Player Movement.cs
@@ -1,23 +1,20 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody)),RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] Camera cam;
     [SerializeField] float cameraSensitivity;
     [SerializeField] float playerSpeed;
     [SerializeField] float verticalCamClamp;
-
     CharacterController charController;
 
-    private Rigidbody rb;
     private Vector2 movementInput;
     private Vector2 inputRot;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
         if (!TryGetComponent<CharacterController>(out charController)) { 
             Debug.LogError($"{name}'s Player Movement Script doesn't have a \"Character Controller\" component!"); 
         }
@@ -28,11 +25,15 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         float delta = Time.deltaTime;
-        //Move Character
+        //Player Movement
         Vector3 moveDir = new Vector3(movementInput.x, 0f, movementInput.y);
         moveDir = transform.TransformDirection(moveDir);
-        charController.Move(moveDir * playerSpeed * delta);
-
+        moveDir *= playerSpeed * delta;
+        //Gravity
+        moveDir.y = Physics.gravity.y * Time.deltaTime;
+        //Apply movement
+        charController.Move(moveDir);
+        
         //Rotate Camera Up/Down
         float camRotX = cam.transform.rotation.eulerAngles.x - inputRot.y * cameraSensitivity * delta;
         cam.transform.localRotation = Quaternion.Euler(camRotX, 0f, 0f);
